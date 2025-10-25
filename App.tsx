927048b990b12e9b2a2f960dd9029933a6f93e56
ANUSHREE-CYBER <anushree.kanoongo@gmail.com>
2025-10-26 01:13:36 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import Group46902 from "./imports/Group46902";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#0e072f] overflow-x-auto overflow-y-auto">
+      <div className="main-container">
+        <Group46902 />
+      </div>
+    </div>
+  );
+}
